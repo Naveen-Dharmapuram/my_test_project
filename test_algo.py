b4ab92494de41e87cc700e641c4184ee6e2397f8
Naveen-Dharmapuram <56017627+Naveen-Dharmapuram@users.noreply.github.com>
2022-08-23 21:25:16 +0530

    Update test_algo.py

--- test_algo.py
+++ test_algo.py
@@ -1,9 +1,9 @@
 import algo
  
 def test_area():
-output = algo.area_of_rectangle(2,5)
-assert output == 10
+    output = algo.area_of_rectangle(2,5)
+    assert output == 10
  
 def test_perimeter():
-output = algo.perimeter_of_rectangle(2,5)
-assert output == 14
+    output = algo.perimeter_of_rectangle(2,5)
+    assert output == 14
