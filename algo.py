ade4910fa8833c321cb942a626069cf003ad6f7f
Naveen-Dharmapuram <56017627+Naveen-Dharmapuram@users.noreply.github.com>
2022-08-23 21:23:28 +0530

    Update algo.py

--- algo.py
+++ algo.py
@@ -1,9 +1,9 @@
 import pytest
  
 def area_of_rectangle(width, height):
-area = width*height
-return area
+    area = width*height
+    return area
  
 def perimeter_of_rectangle(width, height):
-perimeter = 2 * (width + height)
-return perimeter
+    perimeter = 2 * (width + height)
+    return perimeter
